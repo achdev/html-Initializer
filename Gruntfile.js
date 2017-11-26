e8638adeabe502de857b655535fc1f46a502fa95
Anouar CHARIJ <m.charij.anouar@gmail.com>
2017-11-26 17:12:07 +0000

    Adding jade & scss plugins & fix scss file names errors
    This commit contain :
    - Adding jade contrib plugin to compile Jade files.
    - Adding scss contrib plugin to compile SCSS files.
    - Fixing SCSS file names errors.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,53 @@ module.exports = function(grunt) {
 	**/	
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		/**
+		*	@module jade
+		*	Compile JADE to HTML
+		**/
+		jade: {
+			compile: {
+				options: {
+					pretty: true,
+					data: { debug: true }
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: '*.jade',
+					dest: 'dist/',
+					ext: '.html'
+				}]
+			}
+		},
+		/**
+		*	@module sass
+		*	Compile SCSS to CSS
+		**/
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'auto',
+					noCache: false,
+					update: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/scss',
+					src: ['*.scss', '**/*.scss', '**/**/*.scss'],
+					dest: 'dist/css',
+					ext: '.css'
+				}]
+			}
+		},
     });
     
     /**
     *	Register tasks
     **/
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', [
+		'jade',
+		'sass',
+	]);
 };
