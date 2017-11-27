e49eb2e8641f47e478a5dbe45f0f19adfc84ac96
Anouar CHARIJ <m.charij.anouar@gmail.com>
2017-11-27 16:12:17 +0000

    Adding the connect & watch plugins
    This commit contain :
    - Adding the connect plugin.
    - Adding the watch plugin.
    - Set configurations.

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,6 +118,38 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
+		/**
+		*	@module connect
+		*	Livereload connect
+		**/
+		connect: {
+			server: {
+				options: {
+					hostname: '*',
+					port: 8000,
+					base: 'dist/',
+					livereload: true,
+					open: true
+				}
+			}
+		},
+		/**
+		*	@module watch
+		*	Watchers	
+		**/
+		watch: {
+			options: {
+				dateFormat: function(time) {
+					grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+					grunt.log.writeln('Waiting for more changes...');
+				},
+				livereload: true
+			},
+			files: {
+				files: ['src/*.jade', 'src/scss/*.scss', 'src/scss/**/*.scss', 'src/scss/**/**/*.scss'],
+				tasks: ['jade', 'prettify', 'sass', 'csscomb', 'cssmin']
+			},
+		}
     });
     
     /**
@@ -130,5 +162,7 @@ module.exports = function(grunt) {
 		'csscomb',
 		'cssmin',
 		'imagemin',
+		'connect',
+		'watch',
 	]);
 };
